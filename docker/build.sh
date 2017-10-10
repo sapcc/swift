7e2fc0abe94c507b92579fc19d073934c684148f
Stefan Majewsky <stefan.majewsky@sap.com>
2017-10-10 10:44:20 +0200

    need netcat for our statsd metrics machinery to work
    Change-Id: If1198592308f704c8483eaf54f4d4c330f4c89f5

--- docker/build.sh
+++ docker/build.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 # install dependencies
 apt-get update
 apt-get dist-upgrade -y
-DEPENDS="netbase ca-certificates python virtualenv sudo rsync gettext liberasurecode1 libffi6 libssl1.1 busybox-syslogd"
+DEPENDS="netbase ca-certificates python virtualenv sudo rsync gettext liberasurecode1 libffi6 libssl1.1 busybox-syslogd netcat"
 MAKEDEPENDS="curl git virtualenv build-essential python-dev liberasurecode-dev libffi-dev libssl-dev"
 apt-get install -y --no-install-recommends ${DEPENDS} ${MAKEDEPENDS}
 
