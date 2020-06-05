3857679a0f210fd77ad6d8280b0bca327b03ae46
Muhammad Talal Anwar <talalanwar@outlook.com>
2020-06-05 17:43:38 +0200

    Remove python3-venv during cleanup

--- docker/build.sh
+++ docker/build.sh
@@ -4,8 +4,8 @@ set -euxo pipefail
 # install dependencies
 apt-get update
 apt-get dist-upgrade -y
-DEPENDS="netbase ca-certificates curl python3 python3-venv sudo rsync gettext liberasurecode1 libffi6 libssl1.1 netcat procps lsof iproute2"
-MAKEDEPENDS="git build-essential python3-dev liberasurecode-dev libffi-dev libssl-dev"
+DEPENDS="netbase ca-certificates curl python3 sudo rsync gettext liberasurecode1 libffi6 libssl1.1 netcat procps lsof iproute2"
+MAKEDEPENDS="git build-essential python3-venv python3-dev liberasurecode-dev libffi-dev libssl-dev"
 apt-get install -y --no-install-recommends ${DEPENDS} ${MAKEDEPENDS}
 
 # create service user
