484d0729bc7ed841abcbe010af89f8e354ea7563
Barun Kumar <barun311@gmail.com>
2026-07-07 12:36:53 +0530

    Dependency fix for python modules

--- docker-sap/build.sh
+++ docker-sap/build.sh
@@ -47,7 +47,7 @@ if [ "${BUILD_MODE}" = sap ]; then
 #  # https://github.com/advisories/GHSA-5cpq-8wj7-hf2v
 #  https://avd.aquasec.com/nvd/cve-2024-26130
 # pyOpenSSL version need to change to support latest cryptography
-  sed -i '/pyOpenSSL===/c\pyOpenSSL===26.0.0' /root/upper-constraints.txt
+  sed -i '/pyOpenSSL===/c\pyOpenSSL===26.2.0' /root/upper-constraints.txt
   sed -i '/msgpack===/c\msgpack===1.1.1' /root/upper-constraints.txt
   sed -i '/lxml===/c\lxml===6.1.0' /root/upper-constraints.txt
   sed -i '/PyJWT===/c\PyJWT===2.10.1' /root/upper-constraints.txt
